d5e4621bb6422d8387086c30eaaac257200b6378
Jason Qiu <jqiu21@protonmail.com>
2023-10-07 17:29:47 -0400

    Add cli tool, pending api endpoint creation

---
+++ frieren-cli/frieren-cli.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import yaml
+import git
+import re
+
+if __name__ == "__main__":
+    repo = git.Repo(os.getcwd(), search_parent_directories=True)
+    os.chdir(repo.git.rev_parse("--show-toplevel"))
+
+    origin = repo.remotes.origin.url[:-4]
+
+    technologies = list(set(map(lambda x: x.strip(), input("Input technologies separated by a comma (','):").split(","))))
+
+    difficulty = -1
+    try:
+        difficulty = int(input("How difficult is your project to contribute to (1-5)? "))
+        if not 0 < difficulty < 6:
+            raise ValueError()
+    except ValueError as e:
+        print("Error: difficulty should be a number from 1-5")
+    
+    desc = input("Enter a brief description of your project: ")
+
+    recommended_issue_labels = list(set(map(lambda x: x.strip(), input("Input any tag/label that would be a good first issue separated by a comma (','):").split(","))))
+
+    fern = {"name": re.search(r"/([^/]*/[^/]*)$","https://github.com/g00gol/frieren").group(1), "technologies": technologies, "difficulty": difficulty, "description": desc, "recommended_issue_labels": recommended_issue_labels}
+
+    with open("open-source.fern", "w+") as f:
+        yaml.dump(fern, f)
+
+    fern['repo_origin'] = origin
+    print(origin)
+    # Make api call
--- update_scripts/src/main.rs
+++
@@ -1,6 +0,0 @@
-use std::fs;
-use std::io::prelude::*;
-
-fn main() {
-    println!("Hello, world!");
-}
