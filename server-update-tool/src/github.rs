f6ab5fb58b83f43cdf6b14046b7fb8aa8d4b00f4
gcarvellas <gcarvel264@gmail.com>
2023-10-07 17:57:11 -0400

    works on my machine!

--- server-update-tool/src/github.rs
+++ server-update-tool/src/github.rs
@@ -1,25 +1,29 @@
 use std::error::Error;
 use url::Url;
 
-fn get_path_segments_from_url(remote_url: &str) -> Result<Vec<&'static str>, Box<dyn Error>> {
+fn get_path_segments_from_url(remote_url: &str) -> Result<Vec<String>, Box<dyn Error>> {
     let url = Url::parse(&remote_url)?;
-    let path_segments = url.path_segments().map(|c| c.collect::<Vec<_>>()).unwrap();
+
+    let path_segments: Vec<String> = match url.path_segments() {
+        Some(segments) => segments.map(ToString::to_string).collect(),
+        None => Vec::new(),
+    };
 
     // https://github.com/{REPO_OWNER}/{REPO_NAME}
     assert_eq!(2, path_segments.len());
 
-    return Ok(path_segments.clone()); 
+    return Ok(path_segments); 
 
 }
 
-fn get_repo_owner_from_url(remote_url: &str) -> Result<&str, Box<dyn Error>> {
+fn get_repo_owner_from_url(remote_url: &str) -> Result<String, Box<dyn Error>> {
     let path_segments = get_path_segments_from_url(remote_url)?;
-    return Ok(path_segments[0]);
+    return Ok(path_segments[0].to_string());
 }
 
-fn get_repo_name_from_url(remote_url: &str) -> Result<&str, Box<dyn Error>> {
+fn get_repo_name_from_url(remote_url: &str) -> Result<String, Box<dyn Error>> {
     let path_segments = get_path_segments_from_url(remote_url)?;
-    return Ok(path_segments[1]);
+    return Ok(path_segments[1].to_string());
 }
 
 pub async fn check_remote(remote_url: String) -> Result<(), Box<dyn Error>> {
