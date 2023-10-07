47285bef264f67f5bc82addbbab782479ab2f7e1
g00gol <jackey2870@gmail.com>
2023-10-07 17:19:12 -0400

    init frontend

---
+++ frontend/app/page.tsx
@@ -0,0 +1,10 @@
+import { Flex, Text, Button } from "@radix-ui/themes";
+
+export default function Home() {
+  return (
+    <Flex direction="column" gap="2">
+      <Text>Hello from Radix Themes :)</Text>
+      <Button>Let's go</Button>
+    </Flex>
+  );
+}
