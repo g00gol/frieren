5364be818b200c1552e99863900ea99fcae06a29
g00gol <jackey2870@gmail.com>
2023-10-07 15:40:19 -0400

    add mongodb connection

---
+++ backend/db/db.go
@@ -0,0 +1,26 @@
+package db
+
+import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var client *mongo.Client
+
+func Connect() error {
+	var err error
+	opts := options.Client().ApplyURI("mongodb://localhost:27017")
+
+	client, err = mongo.Connect(context.Background(), opts)
+	return err
+}
+
+func GetClient() *mongo.Client {
+	return client
+}
+
+func Disconnect() error {
+	return client.Disconnect(context.Background())
+}
--- backend/main.go
+++ backend/main.go
@@ -4,9 +4,11 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/g00gol/frieren/backend/routes"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+
+	"github.com/g00gol/frieren/backend/db"
+	"github.com/g00gol/frieren/backend/routes"
 )
 
 func main() {
@@ -15,6 +17,15 @@ func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	routes.RegisterRoutes(r)
+
+	// Connect to MongoDB
+	err := db.Connect()
+	defer db.Disconnect()
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		log.Println("Connected to MongoDB")
+	}
 
 	log.Println("Starting server on port " + port)
 	http.ListenAndServe(":"+port, r)
