8282eb74a4a480b1e991022862198c3a97b5a720
Matt Turner <mdturner1026@gmail.com>
2023-10-07 18:00:19 -0400

    controller updated

--- backend/controllers/repos-controller.go
+++ backend/controllers/repos-controller.go
@@ -1,11 +1,19 @@
 package controllers
 
 import (
+	"context"
+	"encoding/json"
 	"net/http"
 
 	"github.com/g00gol/frieren/backend/db"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 func GetRepos(w http.ResponseWriter, r *http.Request) {
 	repos := db.GetRepos()
+	var result bson.M
+	repos.FindOne(context.TODO(), bson.M{}).Decode(&result)
+	jsonData, _ := json.MarshalIndent(result, "", "    ")
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsonData)
 }
