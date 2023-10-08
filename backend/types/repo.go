7610e6f05246726a7a780fd639385240fbad1ed9
g00gol <jackey2870@gmail.com>
2023-10-08 01:51:29 -0400

    adjusted repo type to support json input

--- backend/types/repo.go
+++ backend/types/repo.go
@@ -3,17 +3,17 @@ package types
 import "time"
 
 type Repo struct {
-	Hash                   string    `bson:"hash"`
-	Name                   string    `bson:"name"`
-	Description            string    `bson:"description"`
-	RepoOrigin             string    `bson:"repo_origin"`
-	FernBranch             string    `bson:"fern_branch"`
-	Languages              []string  `bson:"languages"`
-	Technologies           []string  `bson:"technologies"`
-	RecommendedIssueLabels []string  `bson:"recommended_issue_labels"`
-	RecommendedIssuesCount int       `bson:"recommended_issues_count"`
-	Difficulty             int       `bson:"difficulty"`
-	LastUpdated            time.Time `bson:"last_updated"` // MongoDB Datetime
-	DateCreated            time.Time `bson:"date_created"` // MongoDB Datetime
-	Stars                  int       `bson:"stars"`
+	Hash                   string    `bson:"hash" json:"hash"`
+	Name                   string    `bson:"name" json:"name"`
+	Description            string    `bson:"description" json:"description"`
+	RepoOrigin             string    `bson:"repo_origin" json:"repo_origin"`
+	FernBranch             string    `bson:"fern_branch" json:"fern_branch"`
+	Languages              []string  `bson:"languages" json:"languages"`
+	Technologies           []string  `bson:"technologies" json:"technologies"`
+	RecommendedIssueLabels []string  `bson:"recommended_issue_labels" json:"recommended_issue_labels"`
+	RecommendedIssuesCount int       `bson:"recommended_issues_count" json:"recommended_issues_count"`
+	Difficulty             int       `bson:"difficulty" json:"difficulty"`
+	LastUpdated            time.Time `bson:"last_updated" json:"last_updated"`
+	DateCreated            time.Time `bson:"date_created" json:"date_created"`
+	Stars                  int       `bson:"stars" json:"stars"`
 }
